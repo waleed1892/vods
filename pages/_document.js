16dfda7d39b79af97b682ee2ca6fd7491fc7efaf
unknown <waleed.zahid@techliance.com>
2021-05-16 21:53:47 +0500

    gogole analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,11 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
+import {ServerStyleSheet as StyledComponentSheets} from 'styled-components'
+import {ServerStyleSheets as MaterialUiServerStyleSheets} from '@material-ui/core/styles'
 import React from "react";
 
 
-
 class MyDocument extends Document {
-    static async getInitialProps (ctx) {
+    static async getInitialProps(ctx) {
         const styledComponentSheet = new StyledComponentSheets()
         const materialUiSheets = new MaterialUiServerStyleSheets()
         const originalRenderPage = ctx.renderPage
@@ -44,6 +43,20 @@ class MyDocument extends Document {
                     <script async
                             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3548998999865028"
                             crossOrigin="anonymous"></script>
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145394651-21"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-145394651-21', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
+                    />
                 </Head>
                 <body>
                 <Main/>

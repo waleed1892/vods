9dd3ea18bc5d7a7615944e2db05c8a1e3ec6f9b5
unknown <waleed.zahid@techliance.com>
2021-05-31 13:15:58 +0500

    purge through postcss

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 // next.config.js
-const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
 module.exports = {
     images: {
         domains: ['static-cdn.jtvnw.net']
     }
 }
-// next.config.js
-module.exports = withCss(
-    withPurgeCss({
-        purgeCssPaths: [
-            "pages/**/*",
-            "components/**/*",
-            "global/**/*", // also scan other-components folder
-        ],
-    })
-);
-
